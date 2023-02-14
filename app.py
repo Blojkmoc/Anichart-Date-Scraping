f0f0f37e1eeba999cd8b50cbe0869ab8e7c24c1e
Blojkmoc <blojkmoc@gmail.com>
2023-02-15 00:28:38 +0800

    refresh data using apscheduler

--- app.py
+++ app.py
@@ -2,14 +2,17 @@ from flask import Flask, render_template
 import time
 import web_scrape
 import read_pickle
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 events = []
 def load_events():
-    # loaded_anime_title = read_pickle.LoadLists('data/anime_title.pkl')
-    # loaded_episode = read_pickle.LoadLists('data/episode.pkl')
-    # loaded_countdown = read_pickle.LoadLists('data/countdown.pkl')
-    loaded_anime_title, loaded_episode, loaded_countdown = web_scrape.run()
+    events.clear()
+    web_scrape.run()
+    loaded_anime_title = read_pickle.LoadLists('data/anime_title.pkl')
+    loaded_episode = read_pickle.LoadLists('data/episode.pkl')
+    loaded_countdown = read_pickle.LoadLists('data/countdown.pkl')
+    # loaded_anime_title, loaded_episode, loaded_countdown = web_scrape.run()
     for i in range(len(loaded_countdown)):
         events.append({
             'title': f'{loaded_anime_title[i]}: {loaded_episode[i]}',
@@ -17,10 +20,15 @@ def load_events():
             'end': ''  
         })
 
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(load_events,'interval',minutes=2)
+sched.start()
 
 @app.route('/')
 def start():
-    load_events()
+    # events.clear()
+    # web_scrape.run()
+    # load_events()
     #print(events)
     return render_template('cal.html',events=events)
 
